dc8baa7a1ff5ee61fb3d29f328bb3c0e4cf47be7
Aaron <85681241+27Aaron@users.noreply.github.com>
2024-05-03 22:51:51 +0800

    P17

---
+++ Java/P17/HelloWorld.java
@@ -0,0 +1,27 @@
+/**
+ * HelloWorld 
+ * @author Aaron
+ * @version 1.0
+ * @since 2024-05-03
+ */
+
+public class HelloWorld {
+
+    /**
+     * 这是一个main方法，是程序的入口
+     * @param args 这个是main方法的参数
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
+
+// 这是一个单行注释
+
+/*
+多行注释
+多行注释
+多行注释
+多行注释
+多行注释
+*/
