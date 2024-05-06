0beed84a9370a09da34acf88565e51f4f2a5c5c6
Aaron <85681241+27Aaron@users.noreply.github.com>
2024-05-06 23:45:31 +0800

    P33 (#18)

---
+++ Java/P33/ShiLiuJinZhiTest02.java
@@ -0,0 +1,13 @@
+package Java.P33;
+/*
+ * 二进制和十六进制之间的转换
+ */
+public class ShiLiuJinZhiTest02 {
+    public static void main(String[] args) {
+        System.out.println(0b10111111); //191
+        System.out.println(0xbf); //191
+
+        System.out.println(0xb1e2);
+        System.out.println(0b1011000111100010);
+    }
+}
